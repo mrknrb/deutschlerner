5616db462909f029a9b5790d971d0c29237e5d9c
mrknrb <mrknrb2@gmail.com>
2023-06-04 15:49:25 +0200

    1

---
+++ src/routes/pages/dict/+page.server.ts
@@ -0,0 +1,6 @@
+import { error } from '@sveltejs/kit';
+
+import { Configuration, OpenAIApi } from 'openai';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ params }) {}
